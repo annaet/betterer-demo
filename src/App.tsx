a8c0f0fd7303235ade34caa9b36fdabaaa0815cc
Anna Thomas <anna.thomas@hudl.com>
2024-08-29 14:17:46 +0100

    Add comment to showcase betterer results file not updating correctly

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+/**
+ * blah blah
+ * @returns
+ */
+
 /* eslint-disable */
 function App() {
   const [count, setCount] = useState(0)
